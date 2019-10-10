576b1f4ba85ae0194990c55509dc332faf97aa69
sibhat <sibhat.temelso@gmail.com>
2019-10-09 21:13:43 -0700

    change bg color theme to default white color.

--- config/theme.js
+++ config/theme.js
@@ -1,5 +1,5 @@
-import {createMuiTheme} from '@material-ui/core/styles';
-import {red} from '@material-ui/core/colors';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
 
 // Create a theme instance.
 const theme = createMuiTheme({
@@ -14,16 +14,9 @@ const theme = createMuiTheme({
 			main: red.A400,
 		},
 		background: {
-			default: '#606060',
-			paper: '#606060',
+			default: '#fff',
 		},
-		text: {
-			disabled: "rgba(0, 0, 0, 0.38)",
-			hint: "rgba(0, 0, 0, 0.38)",
-			primary: "rgba(255, 255, 255, 0.87)",
-			secondary: "rgba(0, 0, 0, 0.54)"
-		}
 	},
 });
-console.log(theme);
+
 export default theme;
